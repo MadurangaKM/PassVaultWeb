d0e8e2a6e78cd46059116bacd3465feb65b27770
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2023-08-26 11:36:43 +0530

    init project

---
+++ PassVaultWeb/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'passvaultweb',
+    appId: '1:188149631076:web:64281a65d7c812ae07f771',
+    storageBucket: 'passvaultweb.appspot.com',
+    apiKey: 'AIzaSyDW1KMWhazs2zzZGBhzp3yR5BbuzSUAE4E',
+    authDomain: 'passvaultweb.firebaseapp.com',
+    messagingSenderId: '188149631076',
+  },
+};
