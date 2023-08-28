751901f642d359e4da5021f7ad76a49ee320e4fc
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2023-08-28 17:41:50 +0530

    password list data binding

--- PassVaultWeb/src/app/password-list/password-list.component.ts
+++ PassVaultWeb/src/app/password-list/password-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PasswordManagerService } from '../password-manager.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-password-list',
@@ -11,13 +13,84 @@ export class PasswordListComponent {
   siteName!: string;
   siteUrl!: string;
   siteImgUrl!: string;
+  passwordList!: Observable<Array<any>>;
+  email!: string;
+  username!: string;
+  password!: string;
+  passwordId!: string;
+  fromState: string = 'Add new';
+  isSuccess: boolean = false;
+  successMsg!: string;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private passwordManagerService: PasswordManagerService
+  ) {
     this.route.queryParams.subscribe((val: any) => {
       this.siteId = val.id;
       this.siteName = val.siteName;
       this.siteUrl = val.siteUrl;
       this.siteImgUrl = val.siteImgUrl;
     });
+    this.loadPasswords();
+  }
+  showAlert(message: string) {
+    this.isSuccess = true;
+    this.successMsg = message;
+  }
+  onSubmit(values: object) {
+    if (this.fromState == 'Add new') {
+      this.passwordManagerService
+        .addPassword(values, this.siteId)
+        .then(() => {
+          this.showAlert('Data saved successfully');
+          this.handleCancel();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (this.fromState == 'Edit') {
+      this.passwordManagerService
+        .updatePassword(this.siteId, this.passwordId, values)
+        .then(() => {
+          this.showAlert('Data edited successfully');
+          this.handleCancel();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+  loadPasswords() {
+    this.passwordList = this.passwordManagerService.loadPasswords(this.siteId);
+  }
+  editPassword(
+    email: string,
+    username: string,
+    password: string,
+    passwordId: string
+  ) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.passwordId = passwordId;
+    this.fromState = 'Edit';
+  }
+  handleCancel() {
+    this.email = '';
+    this.username = '';
+    this.password = '';
+    this.passwordId = '';
+    this.fromState = 'Add new';
+  }
+  deletePassword(passwordId: string) {
+    this.passwordManagerService
+      .deletePassword(this.siteId, passwordId)
+      .then(() => {
+        this.showAlert('Data deleted successfully');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
--- PassVaultWeb/src/app/password-manager.service.ts
+++ PassVaultWeb/src/app/password-manager.service.ts
@@ -32,4 +32,30 @@ export class PasswordManagerService {
     const docInstance = doc(this.firestore, 'sites', id);
     return deleteDoc(docInstance);
   }
+  //password queries
+
+  addPassword(data: object, siteId: string) {
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return addDoc(dbInstance, data);
+  }
+  loadPasswords(siteId: string) {
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return collectionData(dbInstance, { idField: 'id' });
+  }
+  updatePassword(siteId: string, passwordId: string, data: object) {
+    const dbInstance = doc(
+      this.firestore,
+      `sites/${siteId}/passwords`,
+      passwordId
+    );
+    return updateDoc(dbInstance, data);
+  }
+  deletePassword(siteId: string, passwordId: string) {
+    const docInstance = doc(
+      this.firestore,
+      `sites/${siteId}/passwords`,
+      passwordId
+    );
+    return deleteDoc(docInstance);
+  }
 }
