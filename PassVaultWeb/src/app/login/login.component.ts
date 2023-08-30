4c788ebcdf28a8801992e0933c0f5a28eed36125
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2023-08-30 10:41:11 +0530

    login inline validations added

--- PassVaultWeb/src/app/login/login.component.ts
+++ PassVaultWeb/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PasswordManagerService } from '../password-manager.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,44 +13,54 @@ export class LoginComponent {
   isSignUp: boolean = false;
   actionLabel = 'Login';
   isLoading: boolean = false;
+  loginForm: FormGroup;
+  formSubmitted: boolean = false;
   constructor(
     private passwordManagerService: PasswordManagerService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {
     if (localStorage.getItem('isLogin') == 'true') {
       this.router.navigate(['/site-list']);
     }
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
   onSubmit(values: any) {
-    if (this.isSignUp) {
-      this.isLoading = true;
-      this.passwordManagerService
-        .signup(values.email, values.password)
-        .then(() => {
-          this.isError = false;
-          localStorage.setItem('isLogin', 'true');
-          this.router.navigate(['/site-list']);
-          this.isLoading = false;
-        })
-        .catch((err) => {
-          this.isError = true;
-          this.isLoading = false;
-        });
-    }
-    if (!this.isSignUp) {
-      this.isLoading = true;
-      this.passwordManagerService
-        .login(values.email, values.password)
-        .then(() => {
-          this.router.navigate(['/site-list']);
-          this.isError = false;
-          localStorage.setItem('isLogin', 'true');
-          this.isLoading = false;
-        })
-        .catch((err) => {
-          this.isError = true;
-          this.isLoading = false;
-        });
+    this.formSubmitted = true;
+    if (this.loginForm.valid) {
+      if (this.isSignUp) {
+        this.isLoading = true;
+        this.passwordManagerService
+          .signup(values.email, values.password)
+          .then(() => {
+            this.isError = false;
+            localStorage.setItem('isLogin', 'true');
+            this.router.navigate(['/site-list']);
+            this.isLoading = false;
+          })
+          .catch((err) => {
+            this.isError = true;
+            this.isLoading = false;
+          });
+      }
+      if (!this.isSignUp) {
+        this.isLoading = true;
+        this.passwordManagerService
+          .login(values.email, values.password)
+          .then(() => {
+            this.router.navigate(['/site-list']);
+            this.isError = false;
+            localStorage.setItem('isLogin', 'true');
+            this.isLoading = false;
+          })
+          .catch((err) => {
+            this.isError = true;
+            this.isLoading = false;
+          });
+      }
     }
   }
   handleSignup(status: string) {
